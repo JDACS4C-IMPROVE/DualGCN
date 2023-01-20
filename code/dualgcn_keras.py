e8af0f738586a12b3591bb44d42deab682c44778
Ruibo <zrbbit@gmail.com>
2023-01-20 07:25:17 -0800

    Candlized

---
+++ code/dualgcn_keras.py
@@ -0,0 +1,106 @@
+import candle
+import os
+
+# Just because the tensorflow warnings are a bit verbose
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# This should be set outside as a user environment variable
+os.environ['CANDLE_DATA_DIR'] = os.environ['HOME'] + '/improve_data_dir'
+
+# file_path becomes the default location of the example_default_model.txt file
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+# Define any needed additional args to ensure all new args are command-line accessible.
+additional_definitions = [
+    {'name':'n_fold',
+     'type':int,
+     'nargs':1,
+     'default': 5,
+     'help':'number of folds in the cross validation'
+    },
+
+    {'name':'max_atoms',
+     'type':int,
+     'nargs':1,
+     'default': 100,
+     'help':'max possible size of molecule graphs'
+    },
+
+    {'name':'use_gexpr',
+     'type':bool,
+     'nargs':1,
+     'default': True,
+     'help':'multiomic option'
+    },
+
+    {'name':'use_cnv',
+     'type':bool,
+     'nargs':1,
+     'default': True,
+     'help':'multiomic option'
+    },
+
+    {'name':'regression',
+     'type':bool,
+     'nargs':1,
+     'default': True,
+     'help':'regression or classification'
+    },
+
+    # Model structure
+    {'name':'drug_GCN_units_list',
+     'type': int,
+     'nargs': "+", 
+    #  'default': [256, 128],
+     'help':'max possible size of molecule graphs'
+    },
+
+    {'name':'cell_feature_fc_units_list',
+     'type': int,
+     'nargs': "+", 
+     'default': [32, 128],
+     'help':'max possible size of molecule graphs'
+    },
+
+    {'name':'fc_units_list',
+     'type': int,
+     'nargs': "+", 
+     'default': [256, 128, 10],
+     'help':'max possible size of molecule graphs'
+    },
+
+    {'name':'cell_line_gcn_units_list',
+     'type': int,
+     'nargs': "+", 
+     'default': [256, 256, 256, 256],
+     'help':'max possible size of molecule graphs'
+    },
+
+    {'name':'universal_dropout',
+     'type':float,
+     'nargs':1,
+     'default': 0.1,
+     'help':'max possible size of molecule graphs'
+    },
+
+    {'name':'fc_layers_dropout',
+     'type':float,
+     'nargs': "+", 
+     'default': [0.3, 0.2, 0],
+     'help':'max possible size of molecule graphs'
+    },
+]
+
+# Define args that are required.
+required = None
+
+
+# Extend candle.Benchmark to configure the args
+class DualGCNBenchmark(candle.Benchmark):
+
+    def set_locals(self):
+        if required is not None:
+            self.required = set(required)
+        if additional_definitions is not None:
+            self.additional_definitions = additional_definitions
+
--- code/model.py
+++ code/model.py
@@ -11,13 +11,20 @@ from keras.optimizers import Adam
 from keras.regularizers import l2
 from layers.graph import GraphLayer,GraphConv
 
-    
+
 class KerasMultiSourceDualGCNModel(object):
     def __init__(self,use_gexpr=True,use_cn=True,regr=True):#
         self.use_gexpr = use_gexpr
         self.use_cn = use_cn
         self.regr = regr
-    def createMaster(self,drug_dim,cell_line_dim,units_list):
+
+    def createMaster(self, drug_dim, cell_line_dim, 
+                    drug_GCN_units_list=[256, 128], 
+                    cell_feature_fc_units_list=[32, 128],
+                    fc_units_list=[256, 128, 10],
+                    cell_line_gcn_units_list=[256, 256, 256, 256],
+                    universal_dropout=0.1,
+                    fc_layers_dropout=[0.3, 0.2, 0]):
         # drug-graph input layer
         drug_feat_input = Input(shape=(None,drug_dim),name='drug_feat_input')
         drug_adj_input = Input(shape=(None,None),name='drug_adj_input')
@@ -27,55 +34,58 @@ class KerasMultiSourceDualGCNModel(object):
         cell_line_adj_input = Input(shape=(None,None),name='cell_line_adj_input')
         
         # drug-GCN
-        GCN_layer = GraphConv(units=units_list[0],step_num=1,name="DrugGraph_1_GCN")([drug_feat_input,drug_adj_input])
+        GCN_layer = GraphConv(units=drug_GCN_units_list[0],step_num=1,name="DrugGraph_1_GCN")([drug_feat_input,drug_adj_input])
         GCN_layer = Activation('relu')(GCN_layer)
         GCN_layer = BatchNormalization()(GCN_layer)
-        GCN_layer = Dropout(0.1,name="DrugGraph_1_out")(GCN_layer)
+        GCN_layer = Dropout(universal_dropout, name="DrugGraph_1_out")(GCN_layer)
         
-        GCN_layer = GraphConv(units=128,step_num=1,name="DrugGraph_last_GCN")([GCN_layer,drug_adj_input])
+        GCN_layer = GraphConv(units=drug_GCN_units_list[1],step_num=1,name="DrugGraph_last_GCN")([GCN_layer,drug_adj_input])
         GCN_layer = Activation('relu')(GCN_layer)
         GCN_layer = BatchNormalization()(GCN_layer)
-        GCN_layer = Dropout(0.1,name="DrugGraph_last_out")(GCN_layer)
+        GCN_layer = Dropout(universal_dropout, name="DrugGraph_last_out")(GCN_layer)
 
         x_drug = GlobalAveragePooling1D(name="DrugGraph_out")(GCN_layer)
 
         # bio-graph GCN
-        cell_line_feat_input_high = Dense(32, activation = 'tanh')(cell_line_feat_input) 
-        cell_line_feat_input_high = Dropout(0.1)(cell_line_feat_input_high)
-        cell_line_feat_input_high = Dense(128, activation = 'tanh')(cell_line_feat_input_high) 
-        cell_line_feat_input_high = Dropout(0.1)(cell_line_feat_input_high) 
+        cell_line_feat_input_high = Dense(cell_feature_fc_units_list[0], activation = 'tanh')(cell_line_feat_input) 
+        cell_line_feat_input_high = Dropout(universal_dropout)(cell_line_feat_input_high)
+        cell_line_feat_input_high = Dense(cell_feature_fc_units_list[1], activation = 'tanh')(cell_line_feat_input_high) 
+        cell_line_feat_input_high = Dropout(universal_dropout)(cell_line_feat_input_high) 
 
-        cell_line_GCN = GraphConv(units=256,step_num=1,name="CelllineGraph_1_GCN")([cell_line_feat_input_high,cell_line_adj_input])
+        cell_line_GCN = GraphConv(units=cell_line_gcn_units_list[0],step_num=1,name="CelllineGraph_1_GCN")([cell_line_feat_input_high,cell_line_adj_input])
         cell_line_GCN = Activation('relu')(cell_line_GCN)
         cell_line_GCN = BatchNormalization()(cell_line_GCN)
-        cell_line_GCN = Dropout(0.1)(cell_line_GCN)
+        cell_line_GCN = Dropout(universal_dropout)(cell_line_GCN)
 
-        cell_line_GCN = GraphConv(units=256,step_num=1)([cell_line_GCN,cell_line_adj_input])
-        cell_line_GCN = Activation('relu')(cell_line_GCN)
-        cell_line_GCN = BatchNormalization()(cell_line_GCN)
-        cell_line_GCN = Dropout(0.1)(cell_line_GCN)
+        if len(cell_line_gcn_units_list) > 1:
+            cell_line_GCN = GraphConv(units=cell_line_gcn_units_list[1],step_num=1)([cell_line_GCN,cell_line_adj_input])
+            cell_line_GCN = Activation('relu')(cell_line_GCN)
+            cell_line_GCN = BatchNormalization()(cell_line_GCN)
+            cell_line_GCN = Dropout(universal_dropout)(cell_line_GCN)
 
-        cell_line_GCN = GraphConv(units=256,step_num=1)([cell_line_GCN,cell_line_adj_input])
-        cell_line_GCN = Activation('relu')(cell_line_GCN)
-        cell_line_GCN = BatchNormalization()(cell_line_GCN)
-        cell_line_GCN = Dropout(0.1)(cell_line_GCN)
+        if len(cell_line_gcn_units_list) > 2:
+            cell_line_GCN = GraphConv(units=cell_line_gcn_units_list[2],step_num=1)([cell_line_GCN,cell_line_adj_input])
+            cell_line_GCN = Activation('relu')(cell_line_GCN)
+            cell_line_GCN = BatchNormalization()(cell_line_GCN)
+            cell_line_GCN = Dropout(universal_dropout)(cell_line_GCN)
 
-        cell_line_GCN = GraphConv(units=256,step_num=1)([cell_line_GCN,cell_line_adj_input])
-        cell_line_GCN = Activation('relu')(cell_line_GCN)
-        cell_line_GCN = BatchNormalization()(cell_line_GCN)
-        cell_line_GCN = Dropout(0.1)(cell_line_GCN)
+        if len(cell_line_gcn_units_list) > 3:
+            cell_line_GCN = GraphConv(units=cell_line_gcn_units_list[3],step_num=1)([cell_line_GCN,cell_line_adj_input])
+            cell_line_GCN = Activation('relu')(cell_line_GCN)
+            cell_line_GCN = BatchNormalization()(cell_line_GCN)
+            cell_line_GCN = Dropout(universal_dropout)(cell_line_GCN)
 
         x_cell_line = GlobalAveragePooling1D(name="CelllineGraph_out")(cell_line_GCN)
 
         x = Concatenate(name="Merge_Drug_Cellline_graphs")([x_cell_line,x_drug])
-        x = Dense(256,activation = 'tanh')(x)
-        x = Dropout(0.3)(x)
-        x = Dense(128,activation = 'tanh')(x)
-        x = Dropout(0.2)(x)
-        x = Dense(10,activation = 'tanh')(x)
-        output = Dense(1,name='output')(x)
+        x = Dense(fc_units_list[0], activation = 'tanh')(x)
+        x = Dropout(fc_layers_dropout[0])(x)
+        x = Dense(fc_units_list[1], activation = 'tanh')(x)
+        x = Dropout(fc_layers_dropout[1])(x)
+        x = Dense(fc_units_list[2], activation = 'tanh')(x)
+        x = Dropout(fc_layers_dropout[2])(x)
+        output = Dense(1, name='output')(x)
 
         model  = Model(inputs=[drug_feat_input,drug_adj_input,cell_line_feat_input,cell_line_adj_input],outputs=output)  
 
         return model
-
