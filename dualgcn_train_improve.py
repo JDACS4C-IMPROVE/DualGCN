504bfb18a0945b72f489f76cc6bf339c1d6ada7d
Cesar Augusto Sanchez Villalobos <ac.cesarasa@lambda0.cels.anl.gov>
2024-08-31 10:48:00 -0700

    Inferencing working properly

--- dualgcn_train_improve.py
+++ dualgcn_train_improve.py
@@ -261,7 +261,7 @@ def ModelTraining(model, X_train, Y_train, validation_data,
     history = model.fit(x=X_train, y=Y_train, batch_size=batch_size, epochs=nb_epoch, validation_data=validation_data, callbacks=None)
     return model, history
 
-def ModelEvaluate(model, X_val, Y_val, data_test_idx_current, eval_batch_size=32):
+def ModelEvaluate(model, X_val, Y_val, data_test_idx_current, params, eval_batch_size=32):
 
     Y_pred = model.predict(X_val, batch_size=eval_batch_size)
     
@@ -381,6 +381,7 @@ def main(params):
                                   X_val=X_test,
                                   Y_val=Y_test,
                                   data_test_idx_current=data_test_idx,
+                                  params=params,
                                   eval_batch_size=batch_size)
     print('Evaluation finished!')
     
